a56d5623e1504fba363f201965a3ab81bceacd6d
Gabriel <bergamaschi.gabriel93@gmail.com>
2024-02-21 11:11:31 -0300

    Resoluçao de exemplos com Inteiros

---
+++ main.py
@@ -0,0 +1,44 @@
+# Exercícios
+
+# Inteiros (int)
+
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+# num1 = int(input("Digite o primeiro número inteiro: "))
+# num2 = int(input("Digite o segundo número inteiro: "))
+num1 = 8  # Exemplo de entrada
+num2 = 12  # Exemplo de entrada
+resultado_soma = num1 + num2
+print("A soma é:", resultado_soma)
+
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# num = int(input("Digite um número: "))
+num = 18  # Exemplo de entrada
+resultado_resto = num % 5
+print("O resto da divisão por 5 é:", resultado_resto)
+
+# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# num1 = int(input("Digite o primeiro número: "))
+# num2 = int(input("Digite o segundo número: "))
+num1 = 5  # Exemplo de entrada
+num2 = 7  # Exemplo de entrada
+resultado_multiplicacao = num1 * num2
+print("O resultado da multiplicação é:", resultado_multiplicacao)
+
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+# num1 = int(input("Digite o primeiro número inteiro: "))
+# num2 = int(input("Digite o segundo número inteiro: "))
+num1 = 20  # Exemplo de entrada
+num2 = 3  # Exemplo de entrada
+resultado_divisao_inteira = num1 // num2
+print("O resultado da divisão inteira é:", resultado_divisao_inteira)
+
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+# num = int(input("Digite um número: "))
+num = 6  # Exemplo de entrada
+resultado_quadrado = num**2
+print("O quadrado do número é:", resultado_quadrado)
